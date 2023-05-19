2507e302e677d423c6c35fd44b2a9aa62e7927e9
Derek Chan <djcderek@gmail.com>
2023-05-18 17:05:35 -0700

    Refactor code by creating drawTile function

--- javascript.js
+++ javascript.js
@@ -10,32 +10,21 @@ sizeButton.textContent = 'Press Me';
 bod.appendChild(sizeButton)
 //overlord.appendChild(sizeButton)
 
-let tileSize = 10;
-for (i = 0; i < Number(tileSize); i ++) {
-    const subcontainer = document.createElement('div');
-    subcontainer.classList.add('subcontainer');
-    //subcontainer.style.height = '20px'
-    container.appendChild(subcontainer)
-
-    for (j = 0; j < Number(tileSize); j++) {
-        const tempDiv = document.createElement('div')
-        tempDiv.style.backgroundColor = 'red';
-        tempDiv.style.width = '10px'
-        tempDiv.style.height = 100%
-        tempDiv.classList.add('tile')
-        subcontainer.appendChild(tempDiv)
-    }
-}
+drawTiles(10);
 
 sizeButton.addEventListener('click', () => {
     container.innerHTML = ''
-    tileSize = prompt('Enter a number')
+    let tileSize = prompt('Enter a number')
+    drawTiles(tileSize);
+})
+
+function drawTiles(tileSize) {
     for (i = 0; i < Number(tileSize); i ++) {
         const subcontainer = document.createElement('div');
         subcontainer.classList.add('subcontainer');
         //subcontainer.style.height = '20px'
         container.appendChild(subcontainer)
-    
+
         for (j = 0; j < Number(tileSize); j++) {
             const tempDiv = document.createElement('div')
             tempDiv.style.backgroundColor = 'red';
@@ -46,9 +35,8 @@ sizeButton.addEventListener('click', () => {
             tempDiv.addEventListener("mouseover", (e) => {
                 e.target.style.backgroundColor = 'black';
             })
+
             subcontainer.appendChild(tempDiv)
         }
     }
-})
-
-
+}
